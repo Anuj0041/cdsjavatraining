1d8fab2e8db581c91cda2b71beb2c0f8e50c5a03
Anuj patel <anujpatelfzd@gmail.com>
2024-07-22 15:56:45 +0530

    Done

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum{
     public static void main(String[] args) {
         int a = 5;
-        float b = 12.5f;
+        float b = 7.5f;
         System.out.println(a+b);
     }
 
---
+++ Voting.java
@@ -0,0 +1,8 @@
+public class Voting{
+    public static void main(String[] args) {
+        int age=21;
+        String output =  (age>=18)?"eligible":"not eligible";
+         System.out.println("sum is:"+ output);
+    }
+  
+}
