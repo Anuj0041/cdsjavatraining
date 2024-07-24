c1127aef04751676db81a98e19dedc5a32b77a50
Anuj patel <anujpatelfzd@gmail.com>
2024-07-24 15:49:38 +0530

    Done

---
+++ javaprogram/src/Practicearray.java
@@ -0,0 +1,8 @@
+public class Practicearray {
+    public static void main(String[] args) {
+        String[] friendsName= {"anuj","aditya","abhay","adarsh","ankit"};
+        for(int i=0;i<friendsName.length;i++){
+            System.out.println(friendsName[i]);
+        }
+    }
+}
