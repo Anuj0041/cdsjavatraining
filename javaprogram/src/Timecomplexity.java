67b8a274df13511830e77c184da9d97bf54bef4c
Anuj patel <anujpatelfzd@gmail.com>
2024-07-25 15:19:21 +0530

    done

--- javaprogram/src/Timecomplexity.java
+++ javaprogram/src/Timecomplexity.java
@@ -1,7 +1,7 @@
 public class Timecomplexity {
     public static void main(String[] args) {
-        int i, n = 16;
-        for (i = 1; i <= n; i = i * 2) {
+        int i, n = 8;
+        for (i = 2; i <= n; i= (int)Math.pow(i,2)) {
             System.out.println("Hello world !!!");
         }
     }
