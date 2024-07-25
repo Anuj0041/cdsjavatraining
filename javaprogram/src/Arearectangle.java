06fdbeea2869932e634ebd3fee15e3c04b52cdf0
Anuj patel <anujpatelfzd@gmail.com>
2024-07-25 15:42:05 +0530

    done

---
+++ javaprogram/src/Arearectangle.java
@@ -0,0 +1,9 @@
+public class Arearectangle {
+    public static void main(String[] args) {
+        System.out.println(arearectangle(4,5));
+    }
+    static int arearectangle(int l , int b){
+        return l*b;
+    }
+
+}
